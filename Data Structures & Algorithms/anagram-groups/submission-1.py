0c6196ac2d061fdba3a3ac698cfb2c1c32a53fc2
Berk Ersoz <67341010+ersozberk@users.noreply.github.com>
2026-04-10 19:03:24 +0300

    Add: anagram-groups - submission-1

---
+++ Data Structures & Algorithms/anagram-groups/submission-1.py
@@ -0,0 +1,21 @@
+from collections import defaultdict
+
+class Solution:
+    def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
+        res = defaultdict(list) 
+
+        for s in strs:
+            # Her kelime için a'dan z'ye 26 karakterlik sayacı sıfırla
+            count = [0] * 26 
+            
+            for char in s:
+                # Karakterin ASCII değerine göre indeksini bul (a=0, b=1...)
+                count[ord(char) - ord('a')] += 1
+            
+            # Listeler hashlenemez, bu yüzden tuple'a çevirip anahtar yapıyoruz
+            res[tuple(count)].append(s)
+        
+        return list(res.values())
+
+
+        
